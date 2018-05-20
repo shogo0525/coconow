d5ccc5782e3326432c8a69ef1e63c510b3c113ae
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2018-05-20 11:01:20 +0900

    Add Top.vue

---
+++ src/components/Top.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul>
+    </ul>
+  </div>
+</template>
+
+<script>
+import db from './firebaseInit'
+export default {
+  name: 'Top',
+  data () {
+    return {
+    }
+  },
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Top from '@/components/Top'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Top',
+      component: Top
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
